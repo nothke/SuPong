55e038d22466c29fe9bec3efb31e459abeedcd71
Ivan Notaros <nothke@gmail.com>
2018-06-23 17:58:14 +0200

    First script!

---
+++ Assets/Scripts/Suicider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suicider : MonoBehaviour
+{
+
+    Rigidbody _rb;
+    Rigidbody rb { get { if (!_rb) _rb = GetComponent<Rigidbody>(); return _rb; } }
+
+    public Transform targetT;
+
+    Vector3 target;
+
+    public float force = 100;
+
+    void Update()
+    {
+        target = targetT.position;
+
+        Vector3 forceV = (transform.position - target).normalized * force;
+        rb.AddForce(forceV);
+    }
+}
