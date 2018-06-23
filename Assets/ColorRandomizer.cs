d179970ef01663f4fa8c54043b3b5e17d648714b
rilane <collab.cat@gmail.com>
2018-06-23 20:56:23 +0200

    a person

---
+++ Assets/ColorRandomizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour {
+    [SerializeField] private Gradient colorWheel;
+    [SerializeField] private Gradient spriteColorWheel;
+
+    void Start() {
+        if (GetComponent<Renderer>().material != null)
+            GetComponent<Renderer>().material.color = colorWheel.Evaluate(Random.Range(0f,1f));
+        
+        Color childSpritesColor = spriteColorWheel.Evaluate(Random.Range(0f, 1f));
+        foreach (SpriteRenderer child in GetComponentsInChildren<SpriteRenderer>())
+        {
+            child.color = childSpritesColor;
+        }
+
+    }
+	
+}
