5b679e9948b9e9173cbb764295117721b5da33b9
rilane <s10474@pjwstk.edu.pl>
2018-07-05 20:09:04 +0200

    autocompletion is working/ put stuff to folders

---
+++ Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour {
+    [SerializeField] private Gradient colorWheel;
+    [SerializeField] private Gradient spriteColorWheel;
+
+    void Start() {
+        Color myColor = colorWheel.Evaluate(Random.Range(0f, 1f)),
+             childSpritesColor = spriteColorWheel.Evaluate(Random.Range(0f, 1f));
+
+        if (GetComponent<Renderer>().material != null)
+            GetComponent<Renderer>().material.color = myColor;
+
+        foreach (Renderer child in GetComponentsInChildren<Renderer>())
+        {
+            if (child is SpriteRenderer)
+                child.material.color = childSpritesColor;
+            else
+                child.material.color = myColor;
+        }
+
+    }
+	
+}
